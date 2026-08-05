4afdc4d5e1705eac4f14209e256f066749c28cff
ebishrimp <ebishrimp6022@gmail.com>
2026-08-05 17:40:54 +0900

    衝突時の反発処理を追加

--- Source/Utils/Collider.go
+++ Source/Utils/Collider.go
@@ -1,11 +1,25 @@
 package Utils
 
 type RectCollider struct {
-	width  float64
-	height float64
+	Width  float64
+	Height float64
 }
 
 // Detect the collision of two objects.
 func Collision(rc RectCollider, pos Position, targetrc RectCollider, targetpos Position) bool {
-	return targetpos.X <= pos.X+rc.width && pos.X <= targetpos.X+targetrc.width && targetpos.Y <= pos.Y+rc.height && pos.Y <= targetpos.Y+targetrc.height
+	left := targetpos.X <= pos.X+rc.Width
+	right := pos.X <= targetpos.X+targetrc.Width
+	top := targetpos.Y <= pos.Y+rc.Height
+	bottom := pos.Y <= targetpos.Y+targetrc.Height
+	return left && right && top && bottom
+}
+
+func Repulsion(rc RectCollider, pos Position, targetrc RectCollider, targetpos Position) {
+	for Collision(rc, pos, targetrc, targetpos) {
+		center := pos.PtoV().Add(Vec2{rc.Width / 2, rc.Height / 2})
+		targetCenter := targetpos.PtoV().Add(Vec2{targetrc.Width / 2, targetrc.Height / 2})
+		direction := center.Sub(targetCenter)
+		direction, _ = direction.Normalize()
+		pos.Move(direction)
+	}
 }
