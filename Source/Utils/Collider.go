c762f92162523cbaea1d1ce5d57ddf6bc8137a96
ebishrimp <ebishrimp6022@gmail.com>
2026-08-05 16:39:19 +0900

    add description

--- Source/Utils/Collider.go
+++ Source/Utils/Collider.go
@@ -5,6 +5,7 @@ type RectCollider struct {
 	height float64
 }
 
+// Detect the collision of two objects.
 func Collision(rc RectCollider, pos Position, targetrc RectCollider, targetpos Position) bool {
 	return targetpos.X <= pos.X+rc.width && pos.X <= targetpos.X+targetrc.width && targetpos.Y <= pos.Y+rc.height && pos.Y <= targetpos.Y+targetrc.height
 }
