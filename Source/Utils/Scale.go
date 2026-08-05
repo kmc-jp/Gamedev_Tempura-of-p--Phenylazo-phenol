ae065bd6a6b8ae77230609e885b970d84b674239
ebishrimp <ebishrimp6022@gmail.com>
2026-08-05 12:06:30 +0900

    Scale struct に元画像高さ/幅を入れるフィールドを追加 大きさ関連を扱いやすくするため

--- Source/Utils/Scale.go
+++ Source/Utils/Scale.go
@@ -2,4 +2,12 @@ package Utils
 
 type Scale struct {
 	Rate float64
+
+	//The width / height of original image. These are not expected to reassign
+	OriginalWidth  float64
+	OriginalHeight float64
+}
+
+func (s *Scale) Size() (float64, float64) {
+	return s.OriginalWidth * s.Rate, s.OriginalHeight * s.Rate
 }
