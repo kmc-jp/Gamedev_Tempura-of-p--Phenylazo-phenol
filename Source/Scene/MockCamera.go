48f3f8f8349521a056b713812e81975a98d599ce
biuntri <biuntri.213@gmail.com>
2026-09-07 18:26:57 +0900

    MockCamera.Draw() を実装
    動くかどうかは未確認

--- Source/Scene/MockCamera.go
+++ Source/Scene/MockCamera.go
@@ -2,6 +2,7 @@ package scene
 
 import (
 	"Gamedev_Tempura-of-p--Phenylazo-phenol/Source/Utils"
+	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
@@ -12,7 +13,18 @@ type MockCamera struct {
 
 // Draw implements [Drawer].
 func (m MockCamera) Draw(screen *ebiten.Image, objects []Entity, center Utils.Position) {
-	panic("unimplemented")
+	for _, obj := range objects {
+		image := obj.Draw()
+		pos := obj.Position()
+
+		posx, posy := pos.X, pos.Y
+		sizex, sizey := image.Bounds().Dx(), image.Bounds().Dy()
+		iposx, iposy := math.Floor(posx-float64(sizex)/2), math.Floor(posy-float64(sizey))
+
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Translate(iposx, iposy)
+		screen.DrawImage(image, op)
+	}
 }
 
 func test() {
