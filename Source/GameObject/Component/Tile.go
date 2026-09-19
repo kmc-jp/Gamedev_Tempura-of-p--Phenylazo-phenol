78289289a190afa721900a35275196d1b19e69de
biuntri <biuntri.213@gmail.com>
2026-09-19 20:44:13 +0900

    軽くComponent.Tileを作った

---
+++ Source/GameObject/Component/Tile.go
@@ -0,0 +1,28 @@
+package component
+
+import (
+	gameobject "Gamedev_Tempura-of-p--Phenylazo-phenol/Source/GameObject"
+	tilemap "Gamedev_Tempura-of-p--Phenylazo-phenol/Source/TileMap"
+	"Gamedev_Tempura-of-p--Phenylazo-phenol/Source/Utils"
+)
+
+type Tile struct {
+}
+
+// test
+var _ gameobject.Component = Tile{}
+
+func (h Tile) Update(active bool) (err error) {
+	// なにもしない
+	return nil
+}
+
+func NewTile(tile tilemap.TileData) Utils.Factory[Tile] {
+	return func() (*Tile, error) {
+		t := Tile{}
+		return &t, nil
+	}
+}
+
+// test
+var _ Utils.Factory[HogeComponent] = NewHogeComponent(nil)
