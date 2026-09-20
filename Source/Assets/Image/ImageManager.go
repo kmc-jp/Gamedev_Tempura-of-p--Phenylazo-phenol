200bfd224531a4e63f74e0c16c891d7ed03e821c
biuntri <biuntri.213@gmail.com>
2026-09-20 14:41:32 +0900

    NewImageManager() を実装

--- Source/Assets/Image/ImageManager.go
+++ Source/Assets/Image/ImageManager.go
@@ -1,4 +1,15 @@
 package imageassets
 
+import "fmt"
+
 type ImageManager struct {
+	asepriteLoader *AsepriteLoader
+}
+
+func NewImageManager() (*ImageManager, error) {
+	al, err := NewAsepriteLoader()
+	if err != nil {
+		return &ImageManager{}, fmt.Errorf("NewAsepriteLoader() でエラーが発生しました。\n%w", err)
+	}
+	return &ImageManager{asepriteLoader: al}, nil
 }
