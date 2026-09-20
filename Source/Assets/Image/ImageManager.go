87477f1172ae8262aa3b93c49cf83967053caf85
biuntri <biuntri.213@gmail.com>
2026-09-20 19:38:26 +0900

    ImageManager.Load() を実装

--- Source/Assets/Image/ImageManager.go
+++ Source/Assets/Image/ImageManager.go
@@ -3,6 +3,8 @@ package imageassets
 import (
 	"fmt"
 	"io/fs"
+
+	"github.com/hajimehoshi/ebiten/v2"
 )
 
 type ImageManager struct {
@@ -16,4 +18,12 @@ func NewImageManager(imageFS fs.FS) (*ImageManager, error) {
 		return &ImageManager{}, fmt.Errorf("NewAsepriteLoader() でエラーが発生しました。\n%w", err)
 	}
 	return &ImageManager{asepriteLoader: al, imageFS: imageFS}, nil
+}
+
+func (im ImageManager) Load(data ImageData) (*ebiten.Image, error) {
+	i, err := im.asepriteLoader.Load(data, im.imageFS)
+	if err != nil {
+		return nil, fmt.Errorf("asepriteLoader.Load() でエラーが発生しました。\n%w", err)
+	}
+	return i, nil
 }
