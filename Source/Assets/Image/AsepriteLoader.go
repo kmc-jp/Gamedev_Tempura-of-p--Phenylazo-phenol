865531b893aa7650c1798e0a488a7376e5889c5b
biuntri <biuntri.213@gmail.com>
2026-09-20 20:47:45 +0900

    Asprite に使う Image も取れるように修正

--- Source/Assets/Image/AsepriteLoader.go
+++ Source/Assets/Image/AsepriteLoader.go
@@ -4,34 +4,42 @@ import (
 	"fmt"
 	"io/fs"
 
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/solarlune/goaseprite"
 )
 
 type AsepriteLoader struct {
-	cache map[string]*goaseprite.File
+	cacheAseprite map[string]*goaseprite.File
+	cacheImage    map[string]*ebiten.Image
 }
 
 func NewAsepriteLoader() (*AsepriteLoader, error) {
 	return &AsepriteLoader{}, nil
 }
 
-func (al AsepriteLoader) Load(data ImageData, fs fs.FS) (*goaseprite.File, error) {
-	image, ok := al.cache[data.Name]
-	if !ok {
-		image, err := al.fetch(data, fs)
+func (al AsepriteLoader) Load(data ImageData, fs fs.FS) (*goaseprite.File, *ebiten.Image, error) {
+	asp, aspOk := al.cacheAseprite[data.Name]
+	img, imgOk := al.cacheImage[data.Name]
+	if !aspOk || !imgOk {
+		asp, img, err := al.fetch(data, fs)
 		if err != nil {
-			return nil, fmt.Errorf("AsepriteLoader.fetch() でエラーが発生しました。\n%w", err)
+			return nil, nil, fmt.Errorf("AsepriteLoader.fetch() でエラーが発生しました。\n%w", err)
 		}
-		al.cache[data.Name] = image
+		al.cacheAseprite[data.Name] = asp
+		al.cacheImage[data.Name] = img
 	}
-	return image, nil
+	return asp, img, nil
 }
 
-func (al AsepriteLoader) fetch(data ImageData, fs fs.FS) (*goaseprite.File, error) {
+func (al AsepriteLoader) fetch(data ImageData, fs fs.FS) (*goaseprite.File, *ebiten.Image, error) {
 	aspFile, err := goaseprite.Open(data.JsonPath, fs)
 	if err != nil {
-		return nil, fmt.Errorf("goaseprite.Open(data.JsonPath) でエラーが発生しました。\n%w", err)
+		return nil, nil, fmt.Errorf("goaseprite.Open(data.JsonPath) でエラーが発生しました。\n%w", err)
 	}
-	al.cache[data.Name] = aspFile
-	return aspFile, nil
+	img, _, err := ebitenutil.NewImageFromFile(aspFile.ImagePath)
+	if err != nil {
+		return nil, nil, fmt.Errorf("ebitenutil.NewImageFromFile(aspFile.ImagePath) でエラーが発生しました。\n%w", err)
+	}
+	return aspFile, img, nil
 }
