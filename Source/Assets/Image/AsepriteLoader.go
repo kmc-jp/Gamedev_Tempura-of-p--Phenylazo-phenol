797747b53c0218f475eef1761f739e3f261f16c6
biuntri <biuntri.213@gmail.com>
2026-09-20 16:07:12 +0900

    NewAsepriteLoader() で mime のシステムに .aseprite を追加するようにした

--- Source/Assets/Image/AsepriteLoader.go
+++ Source/Assets/Image/AsepriteLoader.go
@@ -1,11 +1,17 @@
 package imageassets
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"mime"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
 
 type AsepriteLoader struct {
 	cache map[string]ebiten.Image
 }
 
 func NewAsepriteLoader() (*AsepriteLoader, error) {
+	// mime のシステムに .aseprite を追加
+	mime.AddExtensionType(".aseprite", "image/aseprite")
 	return &AsepriteLoader{}, nil
 }
