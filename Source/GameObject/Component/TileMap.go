ed2706bbc5820096ba12908429b7127d9db4e06e
biuntri <biuntri.213@gmail.com>
2026-09-20 10:15:48 +0900

    NewTileMap() を実装

--- Source/GameObject/Component/TileMap.go
+++ Source/GameObject/Component/TileMap.go
@@ -20,6 +20,20 @@ func (h TileMap) Update(active bool) (err error) {
 }
 
 func NewTileMap(mapdata tilemap.TileMapData) Utils.Factory[TileMap] {
+	c := len(mapdata.MapData)
+	tilelist := make(map[tilemap.TilePosition]*Tile, c)
+
+	for pos, tile := range mapdata.MapData {
+		t, err := NewTile(tile)()
+		if err != nil {
+			return func() (*TileMap, error) {
+				return nil, fmt.Errorf("NewTile() でエラーが発生しました。\n%w", err)
+			}
+		}
+
+		// tilelist に登録
+		tilelist[pos] = t
+	}
 	t := TileMap{}
 	return func() (*TileMap, error) {
 		return &t, nil
