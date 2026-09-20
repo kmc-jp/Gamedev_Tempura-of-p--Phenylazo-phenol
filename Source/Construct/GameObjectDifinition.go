83f07de6fd7c076fdde169430aa028179ad5e33e
biuntri <biuntri.213@gmail.com>
2026-09-20 11:16:00 +0900

    NewTileMapObject() を実装

--- Source/Construct/GameObjectDifinition.go
+++ Source/Construct/GameObjectDifinition.go
@@ -31,5 +31,51 @@ func NewTileObject(name string, data tilemap.TileData, scene *scene.PlayScene, N
 	}
 }
 
+func NewTileMapObject(name string, mapdata tilemap.TileMapData, PlScene *scene.PlayScene, NewTransform func(Utils.Position) Utils.Factory[gameobject.Transform], NewRender Utils.Factory[gameobject.Render]) Utils.Factory[gameobject.GameObject] {
+	o, err := gameobject.NewGameObject(name, PlScene, NewTransform(Utils.NewZeroVec2().VtoP()), NewRender)()
+	if err != nil {
+		return func() (*gameobject.GameObject, error) {
+			return o, fmt.Errorf("newStdObject() でエラーが発生しました。\n%w", err)
+		}
+	}
+
+	tm, err := component.NewTileMap(mapdata)()
+	if err != nil {
+		return func() (*gameobject.GameObject, error) {
+			return o, fmt.Errorf("NewTileMap() でエラーが発生しました。\n%w", err)
+		}
+	}
+	err = o.AddComponent(tm)
+	if err != nil {
+		return func() (*gameobject.GameObject, error) {
+			return o, fmt.Errorf("GameObject.AddComponent() でエラーが発生しました。\n%w", err)
+		}
+	}
+
+	// タイル配置
+	newTileObj := func(
+		name string,
+		data tilemap.TileData,
+		Scene *scene.PlayScene,
+		transformFactry Utils.Factory[gameobject.Transform],
+	) Utils.Factory[gameobject.GameObject] {
+		return func() (*gameobject.GameObject, error) {
+			toFactory := NewTileObject(name, data, Scene, transformFactry, NewRender)
+			return toFactory()
+		}
+	}
+
+	err = tm.SetGameObject(PlScene, newTileObj, NewTransform)
+	if err != nil {
+		return func() (*gameobject.GameObject, error) {
+			return o, fmt.Errorf("TileMap.SetGameObject() でエラーが発生しました。\n%w", err)
+		}
+	}
+
+	return func() (*gameobject.GameObject, error) {
+		return o, nil
+	}
+}
+
 // test
 var _ Utils.Factory[gameobject.GameObject] = NewTileObject("", tilemap.TileData{}, nil, nil, nil)
