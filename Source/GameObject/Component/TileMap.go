4617f6111f36ed1847701898e5aa7328519e0fe2
biuntri <biuntri.213@gmail.com>
2026-09-20 22:50:22 +0900

    TileMapData.MapData の型を修正

--- Source/GameObject/Component/TileMap.go
+++ Source/GameObject/Component/TileMap.go
@@ -26,7 +26,7 @@ func NewTileMap(mapdata tilemap.TileMapData) Utils.Factory[*TileMap] {
 	tilelist := make(map[tilemap.TilePosition]*Tile, c)
 
 	for pos, tile := range mapdata.MapData {
-		t, err := NewTile(tile)()
+		t, err := NewTile(*tile)()
 		if err != nil {
 			return func() (*TileMap, error) {
 				return nil, fmt.Errorf("NewTile() でエラーが発生しました。\n%w", err)
