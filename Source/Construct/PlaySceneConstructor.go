a3435aec13e10a3a135c89fbcd227103ac61676c
biuntri <biuntri.213@gmail.com>
2026-09-23 12:00:48 +0900

    タイルマップのサンプルを書いた

--- Source/Construct/PlaySceneConstructor.go
+++ Source/Construct/PlaySceneConstructor.go
@@ -2,6 +2,7 @@ package construct
 
 import (
 	assets "Gamedev_Tempura-of-p--Phenylazo-phenol/Source/Assets"
+	imageassets "Gamedev_Tempura-of-p--Phenylazo-phenol/Source/Assets/Image"
 	gameobject "Gamedev_Tempura-of-p--Phenylazo-phenol/Source/GameObject"
 	component "Gamedev_Tempura-of-p--Phenylazo-phenol/Source/GameObject/Component"
 	scene "Gamedev_Tempura-of-p--Phenylazo-phenol/Source/Scene"
@@ -21,11 +22,41 @@ func PlaySceneConstruct(assetsManager *assets.AssetsManager) Utils.Factory[*scen
 			return playscene, fmt.Errorf("NewPlayScene() でエラーが発生しました。\n%w", err)
 		}
 	}
+
+	ShelfImgData, err := imageassets.NewImageData("Shelf", "みんげー_棚.png", "みんげー_棚.json", assetsManager.Image.ImageFS)
+	if err != nil {
+		return func() (*scene.PlayScene, error) {
+			return playscene, fmt.Errorf("imageassets.NewImageData(\"Shelf\", \"みんげー_棚.png\", \"みんげー_棚.json\", assetsManager.Image.ImageFS) でエラーが発生しました。\n%w", err)
+		}
+	}
+	ShelfTileData, _ := tilemap.NewTileData("Shelf", ShelfImgData)
+
 	mapdata := tilemap.TileMapData{
-		MapData: map[tilemap.TilePosition]tilemap.TileData{},
+		MapData: map[tilemap.TilePosition]*tilemap.TileData{
+			{X: 0, Y: 0}: ShelfTileData,
+			{X: 1, Y: 0}: ShelfTileData,
+			{X: 2, Y: 0}: ShelfTileData,
+			{X: 3, Y: 0}: ShelfTileData,
+			{X: 4, Y: 0}: ShelfTileData,
+			{X: 5, Y: 0}: ShelfTileData,
+			{X: 6, Y: 0}: ShelfTileData,
+			{X: 7, Y: 0}: ShelfTileData,
+			{X: 8, Y: 0}: ShelfTileData,
+			{X: 9, Y: 0}: ShelfTileData,
+			{X: 0, Y: 1}: ShelfTileData,
+			{X: 1, Y: 1}: ShelfTileData,
+			{X: 2, Y: 1}: ShelfTileData,
+			{X: 3, Y: 1}: ShelfTileData,
+			{X: 4, Y: 1}: ShelfTileData,
+			{X: 5, Y: 1}: ShelfTileData,
+			{X: 6, Y: 1}: ShelfTileData,
+			{X: 7, Y: 1}: ShelfTileData,
+			{X: 8, Y: 1}: ShelfTileData,
+			{X: 9, Y: 1}: ShelfTileData,
+		},
 		TileSize: Utils.Vec2{
-			X: 40,
-			Y: 40,
+			X: 54,
+			Y: 54,
 		},
 	}
 	NewTransform := func(position Utils.Position) Utils.Factory[gameobject.Transform] {
