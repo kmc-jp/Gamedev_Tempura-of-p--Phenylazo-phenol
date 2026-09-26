b677a5654fbbcf10fee4dc63d71f7e3fe75ff9e3
biuntri <biuntri.213@gmail.com>
2026-09-27 02:47:12 +0900

    タイルマップのデータの読み込み試験

--- Source/Construct/PlaySceneConstructor.go
+++ Source/Construct/PlaySceneConstructor.go
@@ -2,13 +2,15 @@ package construct
 
 import (
 	assets "Gamedev_Tempura-of-p--Phenylazo-phenol/Source/Assets"
-	imageassets "Gamedev_Tempura-of-p--Phenylazo-phenol/Source/Assets/Image"
+	serialize "Gamedev_Tempura-of-p--Phenylazo-phenol/Source/Assets/Serialize"
+	serializetarget "Gamedev_Tempura-of-p--Phenylazo-phenol/Source/Assets/Serialize/SerializeTarget"
 	gameobject "Gamedev_Tempura-of-p--Phenylazo-phenol/Source/GameObject"
 	component "Gamedev_Tempura-of-p--Phenylazo-phenol/Source/GameObject/Component"
 	scene "Gamedev_Tempura-of-p--Phenylazo-phenol/Source/Scene"
 	tilemap "Gamedev_Tempura-of-p--Phenylazo-phenol/Source/TileMap"
 	"Gamedev_Tempura-of-p--Phenylazo-phenol/Source/Utils"
 	"fmt"
+	"reflect"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/solarlune/goaseprite"
@@ -23,42 +25,28 @@ func PlaySceneConstruct(assetsManager *assets.AssetsManager) Utils.Factory[*scen
 		}
 	}
 
-	ShelfImgData, err := imageassets.NewImageData("Shelf", "みんげー_棚.png", "みんげー_棚.json", assetsManager.Image)
+	serimanager := assetsManager.Serialize
+	shelfdata, err := serialize.NewSerializeData("shelf", reflect.TypeFor[serializetarget.TileMapData](), "TileMap.toml", serimanager)
 	if err != nil {
 		return func() (*scene.PlayScene, error) {
-			return playscene, fmt.Errorf("imageassets.NewImageData(\"Shelf\", \"みんげー_棚.png\", \"みんげー_棚.json\", assetsManager.Image.ImageFS) でエラーが発生しました。\n%w", err)
+			return playscene, fmt.Errorf("serialize.NewSerializeData(\"shelf\", reflect.TypeFor[serializetarget.TileMapData](), \"TileMap.toml\", serimanager) でエラーが発生しました。\n%w", err)
 		}
 	}
-	ShelfTileData, _ := tilemap.NewTileData("Shelf", ShelfImgData)
-
-	mapdata := tilemap.TileMapData{
-		MapData: map[tilemap.TilePosition]*tilemap.TileData{
-			{X: 0, Y: 0}: ShelfTileData,
-			{X: 1, Y: 0}: ShelfTileData,
-			{X: 2, Y: 0}: ShelfTileData,
-			{X: 3, Y: 0}: ShelfTileData,
-			{X: 4, Y: 0}: ShelfTileData,
-			{X: 5, Y: 0}: ShelfTileData,
-			{X: 6, Y: 0}: ShelfTileData,
-			{X: 7, Y: 0}: ShelfTileData,
-			{X: 8, Y: 0}: ShelfTileData,
-			{X: 9, Y: 0}: ShelfTileData,
-			{X: 0, Y: 1}: ShelfTileData,
-			{X: 1, Y: 1}: ShelfTileData,
-			{X: 2, Y: 1}: ShelfTileData,
-			{X: 3, Y: 1}: ShelfTileData,
-			{X: 4, Y: 1}: ShelfTileData,
-			{X: 5, Y: 1}: ShelfTileData,
-			{X: 6, Y: 1}: ShelfTileData,
-			{X: 7, Y: 1}: ShelfTileData,
-			{X: 8, Y: 1}: ShelfTileData,
-			{X: 9, Y: 1}: ShelfTileData,
-		},
-		TileSize: Utils.Vec2{
-			X: 54,
-			Y: 54,
-		},
+	mapdataAny, err := assetsManager.Serialize.Load(*shelfdata)
+	if err != nil {
+		return func() (*scene.PlayScene, error) {
+			return playscene, fmt.Errorf("assetsManager.Serialize.Load(*shelfdata) でエラーが発生しました。\n%w", err)
+		}
 	}
+	mapdataDeserialized := mapdataAny.(serializetarget.TileMapData)
+	mapdataDeserialized.Init("TileMap.toml", assetsManager.Image)
+	mapdataConstructed, err := mapdataDeserialized.Construct()
+	if err != nil {
+		return func() (*scene.PlayScene, error) {
+			return playscene, fmt.Errorf("mapdataDeserialized.Construct() でエラーが発生しました。\n%w", err)
+		}
+	}
+
 	NewTransform := func(position Utils.Position) Utils.Factory[gameobject.Transform] {
 		return func() (gameobject.Transform, error) {
 			tf, err := component.NewStdTransform(position)()
@@ -68,7 +56,7 @@ func PlaySceneConstruct(assetsManager *assets.AssetsManager) Utils.Factory[*scen
 	NewRenderer := func(asp *goaseprite.File, img *ebiten.Image) Utils.Factory[gameobject.Render] {
 		return Utils.CastFactory[*component.StdRender, gameobject.Render](component.NewStdRender(asp, img))
 	}
-	tilemapobj, err := NewTileMapObject("TileMapObj", mapdata, playscene, NewTransform, NewRenderer, assetsManager)()
+	tilemapobj, err := NewTileMapObject("TileMapObj", mapdataConstructed.(tilemap.TileMapData), playscene, NewTransform, NewRenderer, assetsManager)()
 	if err != nil {
 		return func() (*scene.PlayScene, error) {
 			return playscene, fmt.Errorf("NewTileMapObject() でエラーが発生しました。\n%w", err)
