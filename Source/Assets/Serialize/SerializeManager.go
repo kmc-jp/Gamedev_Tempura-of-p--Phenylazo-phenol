83664df79ab2ed014a25238998392f57bdda2b11
biuntri <biuntri.213@gmail.com>
2026-09-26 16:03:21 +0900

    typo

--- Source/Assets/Serialize/SerializeManager.go
+++ Source/Assets/Serialize/SerializeManager.go
@@ -20,15 +20,15 @@ func NewSerializeManager(serializeFS fs.FS) (*SerializeManager, error) {
 	return &SerializeManager{tomlLoader: tl, SerializeFS: serializeFS}, nil
 }
 
-func (im SerializeManager) Load(data SerializeData) (any, error) {
-	item, err := im.tomlLoader.Load(data, im.SerializeFS)
+func (sm SerializeManager) Load(data SerializeData) (any, error) {
+	item, err := sm.tomlLoader.Load(data, sm.SerializeFS)
 	if err != nil {
 		return nil, fmt.Errorf("tomlLoader.Load() でエラーが発生しました。\n%w", err)
 	}
 	return item, nil
 }
 
-func (im SerializeManager) CheckAndCleanPath(path string) (string, error) {
+func (sm SerializeManager) CheckAndCleanPath(path string) (string, error) {
 	if len(strings.TrimSpace(path)) == 0 {
 		return path, fmt.Errorf("path が空です。")
 	}
@@ -39,7 +39,7 @@ func (im SerializeManager) CheckAndCleanPath(path string) (string, error) {
 	}
 	cleanedpath = strings.TrimPrefix(cleanedpath, "/")
 
-	info, err := fs.Stat(im.SerializeFS, cleanedpath)
+	info, err := fs.Stat(sm.SerializeFS, cleanedpath)
 	if err != nil {
 		return cleanedpath, fmt.Errorf("path \"%s\" が使用できません。\n%w", path, err)
 	}
